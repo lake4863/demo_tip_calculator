924c497b85b034eb77341801792231c1627e4e2f
QIANG LI <2001039943@mwce111-11.local>
2020-01-23 19:31:01 -0800

    Initial Commit

---
+++ tip_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip_calculator
+//
+//  Created by QIANG LI on 1/23/20.
+//  Copyright © 2020 QIANG LI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ tip_calculatorTests/tip_calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  tip_calculatorTests.swift
+//  tip_calculatorTests
+//
+//  Created by QIANG LI on 1/23/20.
+//  Copyright © 2020 QIANG LI. All rights reserved.
+//
+
+import XCTest
+@testable import tip_calculator
+
+class tip_calculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
